269ec0eaf663e83a94c45c81525057dc82e1bd83
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-06-02 12:42:57 +0530

    new change

--- src/Component/Formdata.js
+++ src/Component/Formdata.js
@@ -21,69 +21,67 @@ const FormTable = () => {
       phone,
     };
     setTableData([...tableData, formData]);
+    resetForm();
+  };
 
+  const resetForm = () => {
+    setName('');
+    setAge('');
+    setAddress('');
+    setEducation('');
+    setPhone('');
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit} style={{width: '400px',padding: '20px',borderRadius:'5px',backgroundColor:'#f1f1f1'
- }}>
+      <form onSubmit={handleSubmit} style={{ width: '400px', padding: '20px', borderRadius: '5px', backgroundColor: '#f1f1f1' }}>
         <div>
-          <label  htmlFor="name" style={{display:'block', marginBottom:'5px',fontWeight:'bold'}}>Name:</label>
-          <input type="text"id="name"value={name}onChange={(event) => setName(event.target.value)}/>
+          <label htmlFor="name"style={{display:'block',
+          marginBottom:'5px',fontWeight:'bold'}} 
+>Name:</label>
+          <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} />
         </div>
         <div>
-          <label htmlFor="age" style={{display:'block',marginBottom:'5px',fontWeight:'bold'}}>Age:</label>
-          <input
-            type="number"
-            id="age"
-            value={age}
-
-            onChange={(e) => setAge(e.target.value)}
-          />
+          <label htmlFor='age'style={{display:'block',
+          marginBottom:'5px',fontWeight:'bold'}}
+>Age:</label>
+          <input type="number" id="age" value={age} onChange={(e) => setAge(e.target.value)} />
         </div>
         <div>
-          <label htmlFor="address" style={{display:'block',
-          marginBottom:'5px',fontWeight:'bold'}}>Address:</label>
-          <input
-            type="text"
-            id="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
+          <label htmlFor="address"style={{display:'block',
+          marginBottom:'5px',fontWeight:'bold'}}
+>Address:</label>
+          <input type="text" id="address" value={address} onChange={(e) => setAddress(e.target.value)}/>
         </div>
         <div>
-          <label htmlFor="education" style={{display:'block',
-          marginBottom:'5px',fontWeight:'bold'}}>Education:</label>
-          <input
-            type="text"
-            id="education"
-            value={education}
-            onChange={(e) => setEducation(e.target.value)}
-          />
+          <label htmlFor="education"style={{display:'block',
+          marginBottom:'5px',fontWeight:'bold'}}
+>Education:</label>
+          <input type="text" id="education" value={education} onChange={(e) => setEducation(e.target.value)} />
         </div>
         <div>
-          <label htmlFor="phone" style={{display:'block',
-          marginBottom:'5px',fontWeight:'bold'}}>Phone:</label>
-          <input
-            type="text"
-            id="phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
+          <label htmlFor="phone"style={{display:'block',
+          marginBottom:'5px',fontWeight:'bold'}}
+>Phone:</label>
+          <input type="text" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}  />
         </div>
-        <button type="submit" style={{display:'block',width:'100%',
-       padding:'10px',marginTop:'10px',backgroundColor:"#4caf50",
-       color:'#fff',border:'none',borderRadius:'3px',cursor:'pointer'
-      
-      }}>Submit</button>
+        <button type="submit">Submit</button>
       </form>
 
-      <button onClick={() => setShowTable(!showTable)} style={{display:'block',width:'30%',
-       padding:'10px',marginTop:'10px',backgroundColor:"#4caf50",
-       color:'#fff',border:'none',borderRadius:'3px',cursor:'pointer'
-      
-      }}>
+      <button
+        onClick={() => setShowTable(!showTable)}
+        style={{
+          display: 'block',
+          width: '30%',
+          padding: '10px',
+          marginTop: '10px',
+          backgroundColor: '#4caf50',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '3px',
+          cursor: 'pointer'
+        }}
+      >
         {showTable ? 'Hide Table' : 'Show Table'}
       </button>
 
@@ -115,5 +113,8 @@ const FormTable = () => {
   );
 };
 
-
 export default FormTable;
+
+
+
+
